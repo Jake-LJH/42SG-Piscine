cc1fc0fbdae664d0063a22d12bd2ab49f0fb4f8c
Jake Lai <jlai@c1r5s6.42singapore.sg>
2023-07-13 20:26:16 +0800

    Update C04/ex05 atoi_base

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: jlai <jlai@student.42singapore.sg>>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 17:10:56 by jlai              #+#    #+#             */
-/*   Updated: 2023/07/12 21:55:07 by jlai             ###   ########.fr       */
+/*   Updated: 2023/07/13 19:35:37 by jlai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,15 @@ int	ft_base(char *base)
 	i = 0;
 	x = 0;
 	base_len = ft_strlen(base);
-	if (base[i] != '\0' && base[i + 1] != '\0')
+	if (base[i] == '\0' || base_len < 2)
 		return (0);
 	while (base[i] != '\0')
 	{
-		if (base[i] == '+' || base[i] == '-')
+		if (base[i] == '+' || base[i] == '-'
+			|| (base[i] >= 9 && base[i] <= 13) || base[i] == 32)
 			return (0);
 		x = i + 1;
-		while (i < base_len)
+		while (base[x])
 		{
 			if (base[i] == base[x])
 				return (0);
@@ -45,6 +46,20 @@ int	ft_base(char *base)
 		i++;
 	}
 	return (i);
+}
+
+int	get_int(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (c == base[i])
+			return (i);
+		i++;
+	}
+	return (-1);
 }
 
 int	ft_atoi_base(char *str, char *base)
@@ -56,7 +71,9 @@ int	ft_atoi_base(char *str, char *base)
 	i = 0;
 	n = 0;
 	count = 0;
-	while (str[i] == 32 || (str[i] >= 9 || str[i] <= 13))
+	if (ft_base(base) == 0)
+		return (0);
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	while (str[i] == '+' || str[i] == '-')
 	{
@@ -64,12 +81,18 @@ int	ft_atoi_base(char *str, char *base)
 			count++;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (get_int(str[i], base) >= 0)
 	{
-		n *= ft_base(base) + str[i] - 48;
+		n = n * ft_base(base) + get_int(str[i], base);
 		i++;
 	}
 	if (!(count % 2))
 		return (n);
 	return (-n);
 }
+/*
+#include <stdio.h>
+int	main()
+{
+	printf("%d",ft_atoi_base("+-+-BBB", "ABCD"));
+}*/
